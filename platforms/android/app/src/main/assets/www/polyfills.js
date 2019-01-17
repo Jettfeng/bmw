fd732971c9af9a2927a17735b6217eacfa2d2737
lxf <1130034920@qq.com>
2019-01-17 17:44:31 +0800

    ddd

--- platforms/android/app/src/main/assets/www/polyfills.js
+++ platforms/android/app/src/main/assets/www/polyfills.js
@@ -5469,7 +5469,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! I:\宝马项目\bmw\APP\APP\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! F:\源码\宝马项目\bmw\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
